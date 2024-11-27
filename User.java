bc6b7543c674fdf2be7ed1d77a0cc91e7fe40519
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-11-27 16:07:13 -0300

    Comentário dos nodes

--- User.java
+++ User.java
@@ -5,35 +5,35 @@ import java.sql.Statement;
 
 public class User {
     public Connection conectarDB() {
-        Connection conn = null;
+        Connection conn = null;//1
         try {
-            Class.forName("com.mysql.Driver.Manager").newInstance();
-            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-            conn = DriverManager.getConnection(url);
-        } catch (Exception e) {
+            Class.forName("com.mysql.Driver.Manager").newInstance();//2
+            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";//3
+            conn = DriverManager.getConnection(url);//4
+        } catch (Exception e) {//5
         }
-        return conn;
+        return conn;//6
     }
 
-    public String nome = "";
-    public boolean result = false;
+    public String nome = "";//7
+    public boolean result = false;//8
 
     public boolean verificarUsuario(String login, String senha) {
-        String sql = "";
-        Connection conn = conectarDB();
+        String sql = "";//9
+        Connection conn = conectarDB();//10
         // instrução sql
-        sql += "selet nome from usuarios ";
-        sql += "where login = " + "'" + login + "'";
-        sql += " and senha = " + "'" + senha + "';";
+        sql += "selet nome from usuarios ";//11
+        sql += "where login = " + "'" + login + "'";//12
+        sql += " and senha = " + "'" + senha + "';";//13
         try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if (rs.next()) {
-                result = true;
-                nome = rs.getString("nome");
+            Statement st = conn.createStatement();//14
+            ResultSet rs = st.executeQuery(sql);//15
+            if (rs.next()) {//16
+                result = true;//17
+                nome = rs.getString("nome");//18
             }
-        } catch (Exception e) {
+        } catch (Exception e) {//19
         }
-        return result;
+        return result;//20
     }
 }
