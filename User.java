c93da44a32162e837aa64f372b38501b6ce722d2
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-11-29 19:13:26 -0300

    Correção da ordem dos nodes

--- User.java
+++ User.java
@@ -5,22 +5,22 @@ import java.sql.Statement;
 
 public class User {
     public Connection conectarDB() {
-        Connection conn = null;//1
+        Connection conn = null;//5
         try {
-            Class.forName("com.mysql.Driver.Manager").newInstance();//2
-            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";//3
-            conn = DriverManager.getConnection(url);//4
-        } catch (Exception e) {//5
+            Class.forName("com.mysql.Driver.Manager").newInstance();//6
+            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";//7
+            conn = DriverManager.getConnection(url);//8
+        } catch (Exception e) {//9
         }
-        return conn;//6
+        return conn;//10
     }
 
-    public String nome = "";//7
-    public boolean result = false;//8
+    public String nome = "";//1
+    public boolean result = false;//2
 
     public boolean verificarUsuario(String login, String senha) {
-        String sql = "";//9
-        Connection conn = conectarDB();//10
+        String sql = "";//3
+        Connection conn = conectarDB();//4
         // instrução sql
         sql += "selet nome from usuarios ";//11
         sql += "where login = " + "'" + login + "'";//12
